96f9a76f5f1f81f1ce91cebfa23c8ccf8bdc85fb
QiIL <qsy_skx_code@qq.com>
2018-01-15 02:31:01 +0800

    banner feature model

---
+++ api/models/banner_features.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Types.ObjectId
+// banner 数据模型对应位置
+const BannerFeatureSchema = new mongoose.Schema({
+  banner: { type: ObjectId, ref: 'banner' }, // 关联banner
+  features: { type: Number, required: true }, // banner功能
+  describe: { type: Number, required: true } // 功能描述
+})
+
+module.exports = mongoose.model('BannerFeature', BannerFeatureSchema)
